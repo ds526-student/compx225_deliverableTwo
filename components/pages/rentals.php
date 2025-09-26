592f5a2de9f68942b23d6ebdc5f1572694730603
ds526-student <damionsklenarsclare@gmail.com>
2025-09-27 01:07:21 +1200

    global styles added

--- components/pages/rentals.php
+++ components/pages/rentals.php
@@ -4,10 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Navbar Component</title>
+        <link rel="stylesheet" href="../../styles/global.css">
         <link rel="stylesheet" href="../../styles/navbar.css">
         <link rel="stylesheet" href="../../styles/footer.css">
     </head>
-    <body>
+    <body class="content">
         <?php require '../sections/navbar.html'; ?>
         <h1>Rentals Page</h1>
         <?php require '../sections/footer.html'; ?>
