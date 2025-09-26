8b20d0025cbc7e33490af191601b85417ca8a978
ds526-student <damionsklenarsclare@gmail.com>
2025-09-26 21:24:56 +1200

    navbar and footer added
    all pages now have a navbar and footer
    
    plus, styling has been done for the footer and navbar

---
+++ components/pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Navbar Component</title>
+        <link rel="stylesheet" href="../../styles/navbar.css">
+        <link rel="stylesheet" href="../../styles/footer.css">
+    </head>
+    <body>
+        <?php require '../sections/navbar.html'; ?>
+        <h1>Welcome to Kiwi Kloset</h1>
+        <?php require '../sections/footer.html'; ?>
+    </body>
+</html>
--- database/db.php
+++ database/db.php
@@ -1,31 +1,32 @@
 <?php
-// Database configuration for XAMPP
-$host = 'localhost';        // host e.g. 'localhost'
-$dbname = 'kiwi_kloset';    // name of the database
-$username = 'root';         // username e.g. 'root'
-$password = '';             // password e.g. ''
 
-// Create MySQLi connection
-$con = mysqli_connect($host, $username, $password, $dbname);
+    // Database configuration for XAMPP
+    $host = 'localhost';        // host e.g. 'localhost'
+    $dbname = 'kiwi_kloset';    // name of the database
+    $username = 'root';         // username e.g. 'root'
+    $password = '';             // password e.g. ''
 
-// Check connection
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+    // Create MySQLi connection
+    $con = mysqli_connect($host, $username, $password, $dbname);
 
-mysqli_set_charset($con, "utf8");
+    // Check connection
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-// echo "Connected successfully to database: " . $dbname;
+    mysqli_set_charset($con, "utf8");
+
+    // echo "Connected successfully to database: " . $dbname;
+    // $sql_server = "learn-mysql.cms.waikato.ac.nz";
+    // $db_username = "amatenga";
+    // $db_password = "my123456sql";
+    // $db_name = "amatenga";  
+    // $conn = mysqli_connect($sql_server, $db_username, $db_password, $db_name);
+    // if($conn == FALSE) 
+    //     die("Error connecting to mysql server :". mysqli_connect_error());
+    
 ?>
 
-    <!-- ?php
+   
 
-        $sql_server = "learn-mysql.cms.waikato.ac.nz";
-        $db_username = "amatenga";
-        $db_password = "my123456sql";
-        $db_name = "amatenga";  
-        $conn = mysqli_connect($sql_server, $db_username, $db_password, $db_name);
-        if($conn == FALSE) 
-            die("Error connecting to mysql server :". mysqli_connect_error());
 
-    ?> -->
