5358631fc05d6ed269e4c6d7d773b5884ee8bf22
ds526-student <damionsklenarsclare@gmail.com>
2025-09-29 00:09:56 +1300

    table that doesn't work
    called the query from index but the table generation doesn't work yet mainly just committing for transfer

--- components/pages/index.php
+++ components/pages/index.php
@@ -1,4 +1,5 @@
 <?php require_once '../../config/db.php'; ?>
+<?php require_once '../../queries/select_queries.php'; ?>
 <!DOCTYPE html>
 <html lang="en-us">
     <head>
@@ -36,6 +37,33 @@
             <br>
             5. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
+
+        <!-- Display Costumes from Database -->
+        <div class="inline-padding">
+            <h2>Available Costumes</h2>
+            <?php
+                
+                $result = getAllCostumes();
+
+                if ($result) {
+                    echo "<table><tr><th>Id</th><th>Is available</th><th>Branch Id</th><th>Name</th><th>Size</th><th>Daily Rate</th><th>Category</th></tr>";
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                        echo "<td>".$row['id']."</td>";
+                        echo "<td>".$row['is_available']."</td>";
+                        echo "<td>".$row['branch_id']."</td>";
+                        echo "<td>".$row['name']."</td>";
+                        echo "<td>".$row['size']."</td>";
+                        echo "<td>$".number_format($row['daily_rate'], 2)."</td>";
+                        echo "<td>".$row['category']."</td>";
+                        echo "</tr>";
+                        echo "</table>"
+                    }
+                } else {
+                    echo "<p>Error: " . mysqli_error($con) . "</p>";
+                }
+            ?>
+        </div>
 
         <!-- Footer -->
         <?php require '../partials/footer.html'; ?>
